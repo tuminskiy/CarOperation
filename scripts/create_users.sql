84585a48e07d23faa2c5fc96f7333b41b8b17da1
tuminskiy <unknowuzvername@gmail.com>
2021-06-06 22:06:50 +0300

    Fix privilegies

--- scripts/create_users.sql
+++ scripts/create_users.sql
@@ -1,10 +1,10 @@
 CREATE ROLE worker_role WITH CREATEROLE;
 GRANT ALL PRIVILEGES ON DATABASE CarOperation TO worker_role;
 
-CREATE ROLE watcher_role;
-GRANT USAGE ON SCHEMA public TO watcher_role;
-GRANT SELECT ON ALL TABLES IN SCHEMA public TO watcher_role;
-GRANT SELECT ON bus_driver_view TO watcher_role;
+CREATE ROLE worker_role WITH CREATEROLE;
+GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO worker_role;
+GRANT ALL PRIVILEGES ON ALL FUNCTIONS IN SCHEMA public TO worker_role;
+GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO worker_role;
 
 CREATE USER user_worker WITH PASSWORD 'zx3021';
 GRANT worker_role TO user_worker;
